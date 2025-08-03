2dee4560f29b6fae61c594e912299f5509c1ed15
Coacoabro <aaLogan9@gmail.com>
2025-08-02 22:43:39 -0400

    New Day

--- src/pages/[genre]/game.js
+++ src/pages/[genre]/game.js
@@ -103,8 +103,7 @@ export default function Game( {genre} ) {
     useEffect(() => {
         if (typeof window === "undefined") return;
 
-        // const today = getToday();
-        const today = "asdf"
+        const today = getToday();
         const lastPlayed = localStorage.getItem(STORAGE_DATE_KEY);
 
         if (!lastPlayed || lastPlayed !== today) {
